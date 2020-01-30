1035657a768e910f8febbcace350d854cb35cba1
Daniel Philippus <dphilippus@mines.edu>
2020-01-30 12:52:31 -0700

    Basic functionality fully working

--- ras/ras.py
+++ ras/ras.py
@@ -32,6 +32,70 @@ xs.setAllManning(ns): set all Manning's ns to a list of ns given
 xs.setMainChannelManning(n): set the main channel n to the given n
 """
 
+from ras.wrapper import RasObject
+
+class Ras(object):
+    """
+    The whole RAS controller.
+    """
+    def __init__(self, projectPath, rasObject = RasObject()):
+        self.ras = rasObject
+        if not (projectPath is None):
+            self.openProject(projectPath)
+        self.rivers = [River(self.ras, river.strip()) for river in self.ras.GetRivers()[1]]
+
+    def openProject(self, path):
+        self.ras.OpenProject(path)
+
+    def river(self, river, geom = None):
+        return [riv for riv in self.rivers if riv.river == river][0]
+
+    def reach(self, river, reach, geom = None):
+        # Geom not implemented yet.
+        return self.river(river, geom).reach(reach)
+
+    def computeSteady(self, plan = None):
+        self.ras.Compute()
+
+    def computeUnsteady(self, plan = None):
+        self.ras.Compute()
+
+    def getSimData(self, river = None, reach = None, rs = None, prof = 1):
+        if river is None:
+            return {riv.river: self.getSimData(riv.river, reach, rs) for riv in self.rivers}
+        elif reach is None:
+            riv = self.river(river)
+            return {rch.reach: self.getSimData(riv.river, rch.reach, rs) for rch in riv.reaches}
+        elif rs is None:
+            riv = self.river(river)
+            rch = self.reach(river, reach)
+            return {xs.rs: self.getSimData(riv.river, rch.reach, xs.rs) for xs in rch.xses}
+        else:
+            riv = self.river(river)
+            rch = riv.reach(reach)
+            xs = rch.xs(rs)
+            sd = self.ras.GetVelDist(xs.riverID, xs.reachID, xs.xsID, 0, prof)
+            # sd: (6x args, (left station), (right station), (conv perc), (area), (wetted perimeter), (flow),
+            # (depth), (velocity))
+            velocity = sd[-1][0]
+            depth = sd[-2][0]
+            flow = sd[-3][0]
+            etc = {
+                "area": sd[-4][0],
+                "wp": sd[-5][0]
+            }
+            return SimData(velocity, depth, flow, etc)
+
+class SimData(object):
+    """
+    Simulation data for a cross section.  "etc" is a dictionary of less relevant data.
+    """
+    def __init__(self, velocity, maxDepth, flow, etc):
+        self.velocity = velocity
+        self.maxDepth = maxDepth
+        self.flow = flow
+        self.etc = etc
+
 class XS(object):
     """
     A cross section.
@@ -47,6 +111,9 @@ class XS(object):
         self.river = river
         self.reach = reach
         self.rs = rs
+        self.riverID = getRiverID(self.ras, self.river)
+        self.reachID = getReachID(self.ras, self.river, self.reach)
+        self.xsID = getXSID(self.ras, self.river, self.reach, self.rs)
 
     def setAllManning(self, ns):
         """
@@ -62,6 +129,36 @@ class XS(object):
         """
         self.ras.SetMannLCR(self.river, self.reach, self.rs, n, n, n)
 
+def getRiverID(ras, river):
+    """
+    Find river ID from river name
+    :param ras: RasObject
+    :param river: river name
+    :return: river id
+    """
+    rivers = ras.GetRivers()[1]
+    for ix, riv in enumerate(rivers):
+        if riv.startswith(river): # starts with because the names often have a bunch of extra spaces
+            return ix + 1
+    return False
+
+def getReachID(ras, river, reach):
+    reaches = ras.GetReaches(getRiverID(ras, river))[2]
+    for ix, rch in enumerate(reaches):
+        if rch.startswith(reach):
+            return ix + 1
+    return False
+
+def getXSID(ras, river, reach, xs):
+    """
+    Count from the top, not the bottom. <not currently>
+    """
+    xses = [xs.strip() for xs in ras.GetNodes(getRiverID(ras, river), getReachID(ras, river, reach))[3]]
+    for (ix, rs) in enumerate(xses):
+        if rs.startswith(xs):
+            return ix + 1
+    return False
+
 class Reach(object):
     """
     A reach.
@@ -70,11 +167,29 @@ class Reach(object):
         self.ras = ras
         self.river = river
         self.reach = reach
-        self.xses = self.getCrossSections()
+        self.riverID = getRiverID(self.ras, self.river)
+        self.reachID = getReachID(self.ras, self.river, self.reach)
+        self.xses = [XS(self.ras, self.river, self.reach, rs) for rs in self.getCrossSections()]
 
     def getCrossSections(self):
-        """
-        Get a list of the cross sections.  First, get integer IDs for river and reach, then call
-        ras.GetNodes(riv, rch).  It'll be the third item, and all of the non-blank ones within.
-        """
+        return [xs.strip() for xs in self.ras.GetNodes(self.riverID, self.reachID)[3]]
+
+    def xs(self, rs):
+        return [xs for xs in self.xses if xs.rs == rs][0]
+
+class River(object):
+    """
+    A river.
+    """
+    def __init__(self, ras, river):
+        self.ras = ras
+        self.river = river
+        self.riverID = getRiverID(self.ras, self.river)
+        self.reaches = [Reach(self.ras, self.river, reach) for reach in self.getReaches()]
+
+    def getReaches(self):
+        return [r.strip() for r in self.ras.GetReaches(self.riverID)[2]]
+
+    def reach(self, reach):
+        return [rch for rch in self.reaches if rch.reach == reach][0]
 
